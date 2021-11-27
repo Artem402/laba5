2b9de4533921e95d3abde5cdcf644243634964a6
Artem <2017sabserj@gmail.com>
2021-11-27 20:33:40 +0200

    Ordinary+Advanced calcs

--- Program.cs
+++ Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace laba5
 {
+    class OrdinaryCalc
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+        public double Mul(double x, double y)
+        {
+            return x * y;
+        }
+        public double Div(double x, double y)
+        {
+            return x / y;
+        }
+
+    }
+    class AdvancedCalc : OrdinaryCalc
+    {
+        public double Sqrt(int x)
+        {
+            return Math.Sqrt(x);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
