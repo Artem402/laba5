51e5c297c71de81ed6330729abe6196041eb583f
Artem <2017sabserj@gmail.com>
2021-11-27 21:12:52 +0200

    almost ready

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ namespace laba5
     }
     class AdvancedCalc : OrdinaryCalc
     {
-        public double Sqrt(int x)
+        public double Sqrt(double x)
         {
             return Math.Sqrt(x);
         }
@@ -37,7 +37,67 @@ namespace laba5
     {
         static void Main(string[] args)
         {
-
+            double x = 0, y = 0;
+            var ord = new OrdinaryCalc();
+            var adv = new AdvancedCalc();
+            while (true)
+            {
+                Console.WriteLine("1 - Обычный калькулятор\n2 - Продвинутый калькулятор\nДругое - выход");
+                var r = Console.ReadLine();
+                if (r == "1")
+                {
+                    try 
+                    { 
+                        Console.WriteLine("Выберите действие +, - , *, / ");
+                        var a = Console.ReadLine();
+                        Console.WriteLine("Введите первое число: ");
+                        x = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите второе число: ");
+                        y = Convert.ToDouble(Console.ReadLine());
+                        if (a == "+") Console.WriteLine($"Ответ: {ord.Add(x, y)}");
+                        else if (a == "-") Console.WriteLine($"Ответ: {ord.Sub(x, y)}");
+                        else if (a == "*") Console.WriteLine($"Ответ: {ord.Mul(x, y)}");
+                        else if (a == "/") 
+                        {
+                            if (y == 0) throw new Exception();
+                            Console.WriteLine($"Ответ: {ord.Div(x, y)}"); 
+                        }
+                        else Console.WriteLine("Действие выбрано неверно");
+                    }
+                    catch { Console.WriteLine("Числа введены неверно"); }
+                }
+                else if (r == "2")
+                {
+                    Console.WriteLine("Выберите действие +, - , *, /, sqrt ");
+                    var a = Console.ReadLine();
+                    try
+                    {
+                        if (a == "sqrt")
+                        {
+                            Console.WriteLine("Введите число: ");
+                            x = Convert.ToDouble(Console.ReadLine());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Введите первое число: ");
+                            x = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Введите второе число: ");
+                            y = Convert.ToDouble(Console.ReadLine());
+                        }
+                        if (a == "+") ord.Add(x, y);
+                        else if (a == "-") Console.WriteLine($"Ответ: {adv.Sub(x, y)}");
+                        else if (a == "*") Console.WriteLine($"Ответ: {adv.Mul(x, y)}");
+                        else if (a == "/")
+                        {
+                            if (y == 0) throw new Exception();
+                            Console.WriteLine($"Ответ: {adv.Div(x, y)}");
+                        }
+                        else if (a == "sqrt") Console.WriteLine($"Ответ: {adv.Sqrt(x)}");
+                        else Console.WriteLine("Действие выбрано неверно");
+                    }
+                    catch { Console.WriteLine("Числа введены неверно"); }
+                }
+            }
         }
     }
 }
